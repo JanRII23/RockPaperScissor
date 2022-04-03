ec91909c0b415f91715cccabbcb3e4bca420026f
Jan <jrejasii@gmail.com>
2022-04-02 20:49:57 -0500

    improved game

--- java.js
+++ java.js
@@ -1,76 +1,111 @@
-const computerPlay = function(){
-    let randomNum = Math.ceil(Math.random() * 3)
-    
-    if (randomNum == 1){
-        return "Rock";
-    }
-    else if (randomNum == 2){
-        return "Paper";
-    }
-    else if (randomNum == 3){
-        return "Scissors";
-    }
+let computerScore = 0;
+let userScore = 0;
+
+const computerScore_span = document.getElementById("computerScore");
+const userScore_span = document.getElementById("userScore");
+
+const scoreBoard_div = document.querySelector(".scoreBoard");
+
+const result_div = document.querySelector(".result");
+
+const liz_div = document.getElementById("liz");
+const spk_div = document.getElementById("spk");
+const rck_div = document.getElementById("rck");
+const scis_div = document.getElementById("scis");
+const pap_div = document.getElementById("pap");
+
+//Above is caching the DOM
+
+//Do not forget to add to the html file, took a while to figure out i forgot to src it
+
+function convertWord(word){
+  if (word === "liz") return "Lizard";
+  if (word === "spk") return "Spock";
+  if (word === "rck") return "Rock";
+  if (word === "scis") return "Scissors";
+  if (word === "pap") return "Paper";
+}
+
+function win(userChoice, computerChoice){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+
+    result_div.innerHTML = `WIN, user chose ${convertWord(userChoice)} -- computer chose ${convertWord(computerChoice)}`;
+   
+}
+
+//I need to add unique description for each identifier
+
+function lose(userChoice, computerChoice){
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    userScore_span.innerHTML = userScore;
+
+    result_div.innerHTML = `LOSE, user chose ${convertWord(userChoice)} -- computer chose ${convertWord(computerChoice)}`;
+}
+
+
+const getComputerChoice = function(){
+
+    const choices = ['liz', 'spk', 'rck', 'scis', 'pap'];
+    const randomNumber = Math.floor(Math.random()*5);
+    return choices[randomNumber];
 
 }
 
-const userOption = function(num){
-    if (num == 1){
-        return "Rock";
-    }
-    else if (num == 2){
-        return "Paper";
-    }
-    else if (num == 3){
-        return "Scissors";
-    }
-}
-
-const gameWinner = function(computerOption, userValue){
-    if (userValue == "Rock" && computerOption == "Scissors"){
-        console.log(`You Win! User ${userValue} beats Computer ${computerOption}`);
-        return 1;
-    }
-    else if (userValue === "Paper" && computerOption === "Rock"){
-        console.log(`You Win! User ${userValue} beats Computer ${computerOption}`);
-        return 1;
-    }
-    else if (userValue === "Scissors" && computerOption === "Paper"){
-        console.log(`You Win! User ${userValue} beats Computer ${computerOption}`);
-        return 1;
-    }
-    else if(userValue == computerOption){
-        console.log("Tie");
-    }
-    else{
-        console.log(`You Lose! Computer ${computerOption} beats User ${userValue}`);
-        return 2;
+const game = function(userChoice){
+    const computerChoice = getComputerChoice();
+    switch(userChoice + computerChoice){
+        case "spkscis":
+        case "spkrck":
+        case "scispap":
+        case "scisliz":
+        case "papspk":
+        case "paprck":
+        case "rckliz":
+        case "rckscis":
+        case "lizspk":
+        case "lizpap":
+            win(userChoice, computerChoice);
+            break;
+        case "lizliz":
+        case "spkspk":
+        case "scisscis":
+        case "pappap":
+        case "rckrck":
+            result_div.innerHTML = "DRAW!";
+            break;
+        default:
+            lose(userChoice, computerChoice);
     }
 }
 
-let userScore = 0,
-    computerScore = 0;
+function main(){
 
-for (let i = 0; i < 5; i++){
-
-    let computerOption = computerPlay();
-    console.log (`Computer chose: ${computerOption}`);
-
-    let userValue = userOption(window.prompt("Enter 1: Rock, 2: Paper, or 3: Scissors"));
-    console.log(`User chose: ${userValue}`);
-
-    let scoreValue = gameWinner(computerOption, userValue);
-    if (scoreValue == 1){
-        userScore++;
-    }else if (scoreValue == 2){
-        computerScore++;
-    }
-    
+    liz_div.addEventListener('click', () => {
+        game("liz");
+    })
+    spk_div.addEventListener('click', () => {
+        
+        game("spk");
+    })
+    rck_div.addEventListener('click', () => {
+        game("rck");
+        
+    })
+    scis_div.addEventListener('click', () => {
+        
+        game("scis");
+    })
+    pap_div.addEventListener('click', () => {
+       
+        game("pap");
+    })
 }
 
-if (userScore > computerScore){
-    console.log("5 Round Winner");
-}else if(userScore < computerScore){
-    console.log("5 Round Loser");
-}else{
-    console.log("5 Round Tie");
-}
+main();
+
+
+
+
